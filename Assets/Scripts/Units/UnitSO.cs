c1c8a5267e0aaa9a9318d8b6d8df4b6a20fd4f81
Connor Ferns <connorferns@gmail.com>
2025-08-20 21:02:07 +0100

    Add Settler unit ScriptableObject and spawn tester
    Introduced a new UnitSO ScriptableObject for defining unit types, including a Settler unit asset. Added a SpawnUnitTester MonoBehaviour and placed it in the SampleScene for testing unit spawning. Updated UnitSO.cs to include unit stats, types, and build costs.

--- Assets/Scripts/Units/UnitSO.cs
+++ Assets/Scripts/Units/UnitSO.cs
@@ -1,6 +1,34 @@
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
+[CreateAssetMenu(menuName = "Kingdom Cards/Unit Type")]
 public class UnitSO : ScriptableObject
 {
-    
+    [System.Serializable]
+    public enum UnitType
+    {
+        Settler,
+        Melee,
+        Ranged,
+        Scout
+    }
+
+    [Header("Basic Unit Info")]
+    public string unitName;
+    public UnitType unitType;
+    public Transform prefab;
+    public Sprite icon;
+
+    [Header("Combat Stats")]
+    public int maxHealth;
+    public int attackDamage;
+    public int attackRange;
+    public int defence;
+
+    [Header("Movement")]
+    public int movementRange = 2;
+
+    [Header("Resource Costs")]
+    public List<ResourceManager.ResourceAmount> buildCosts  = new List<ResourceManager.ResourceAmount>();
 }
